f9851739e567af83872d267db9a8809d97a3c1d3
slavic-sky <44680193+slavic-sky@users.noreply.github.com>
2020-10-01 15:35:04 +0300

    navigation ids fix

--- app/src/main/java/ru/slavicsky/chuckjokesapp/MainActivity.kt
+++ app/src/main/java/ru/slavicsky/chuckjokesapp/MainActivity.kt
@@ -26,8 +26,8 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.jokes,
-                R.id.web
+                R.id.navigation_jokes,
+                R.id.navigation_web
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
