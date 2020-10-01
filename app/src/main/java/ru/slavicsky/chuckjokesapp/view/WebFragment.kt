95a30762caf362059fe993d37467b1beb2bed4c2
slavic-sky <44680193+slavic-sky@users.noreply.github.com>
2020-10-01 15:55:34 +0300

    webview fix

--- app/src/main/java/ru/slavicsky/chuckjokesapp/view/WebFragment.kt
+++ app/src/main/java/ru/slavicsky/chuckjokesapp/view/WebFragment.kt
@@ -1,15 +1,16 @@
 package ru.slavicsky.chuckjokesapp.view
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.slavicsky.chuckjokesapp.MainActivity
 import ru.slavicsky.chuckjokesapp.R
 
 @AndroidEntryPoint
@@ -23,12 +24,14 @@ class WebFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        (activity as MainActivity).titleMain.text = getString(R.string.title_web)
 
         val root = inflater.inflate(R.layout.fragment_web, container, false)
-
         val mWebView = root.findViewById(R.id.web_view) as WebView
+
         mWebView.settings.javaScriptEnabled = true
         mWebView.webViewClient = WebViewClient()
+        mWebView.settings.defaultZoom = WebSettings.ZoomDensity.FAR;
         mWebView.loadUrl(API_URL)
         return root
     }
